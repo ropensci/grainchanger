960a8b50673bb6a65a4f9f4d1983410e1dedc556
laurajanegraham <laurajaneegraham@gmail.com>
2017-10-09 21:17:09 +0100

    change output of main function to return only values rather than spatial dataframe

--- R/winmoveR_functions.R
+++ R/winmoveR_functions.R
@@ -88,12 +88,11 @@ winmove_cellmean <- function(winmove_cellr) {
 #'@param type The shape of the moving window
 #'@param fn The function to apply 
 #'@param ... further arguments passed to or from other methods
-#'@return A raster with the moving window values calculated
+#'@return Numeric vector containing moving window values calculated for each grid cell
 #'@export
 
 winmove_upscale <- function(grid, dat, radius, type, fn, ...) {
   cell_div <- lapply(1:nrow(grid), winmove_cell, grid, dat, radius, type, fn, ...)
   cell_mean <- plyr::ldply(cell_div, winmove_cellmean)
-  grid@data <- data.frame(grid@data, cell_mean)
-  return(grid)
+  return(cell_mean)
 }
