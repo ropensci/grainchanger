cfeb8761f1d302148871e6f51ea13cb26e2544fe
laurajanegraham <laurajaneegraham@gmail.com>
2018-04-11 17:39:21 +0100

    update package to include proportion calculation

--- R/winmove_nbrhd.R
+++ R/winmove_nbrhd.R
@@ -4,8 +4,8 @@
 #'@param dat The raster dataset on which to calculate the moving window function
 #'@param radius The radius of the moving window
 #'@param type The shape of the moving window
-#'@param fn The function to apply 
-#'@param ... further arguments passed to or from other methods
+#'@param fn The function to apply - inbuilt functions are currently `diversity` (Shannon evenness of a given list of land-covers) and `prop` (proportion of a given land-cover). For inbuilt functions, land-cover should be passed in as an argument `lc_class`
+#'@param ... further arguments passed to or from other methods (`lc_class` as a list for `diversity` or integer for `prop`)
 #'@return A raster with the moving window values calculated
 #'@export
 
@@ -15,6 +15,8 @@ winmove_nbrhd <- function(dat, radius, type=c("circle", "rectangle"), fn, ...) {
     out <- diversity_nbrhd(dat, wdw, ...)
   } else if(fn == "mean") {
     out <- raster::focal(dat, wdw)
+  } else if(fn == "prop") {
+    out <- prop_nbrhd(dat, wdw, ...)
   } else {
     # for all other functions, we just want weights of 1
     wdw <- ifelse(wdw > 0, 1, NA)
