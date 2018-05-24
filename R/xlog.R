d24d325028fdaa5021cafd49e0b50c27cfe97409
laurajanegraham <laurajaneegraham@gmail.com>
2018-05-24 15:43:26 +0100

    remove unused functions

--- R/xlog.R
+++
@@ -1,8 +0,0 @@
-#' Upscale the moving window value
-#' 
-#' Calculate xlogx of a given value for use in `diversity()` and `diversity_nbrhd()` functions
-#'@param x numeric
-#'@return numeric vector
-
-xlog <- function(x) ifelse(x==0, 0, x*log(x))
-
