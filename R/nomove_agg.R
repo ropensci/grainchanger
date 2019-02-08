8ec49aa114d45f703860d9e4310b15ff1b2ab87f
laurajanegraham <laurajaneegraham@gmail.com>
2019-02-08 19:09:16 +0000

    update to include sf

--- R/nomove_agg.R
+++ R/nomove_agg.R
@@ -7,24 +7,21 @@
 #'@param ... further arguments passed to or from other methods
 #'@return Raster (if input is Raster) or numeric vector (if input is SpatialPolygon) containing values calculated for each coarser cell
 #'@export
-nomove_agg <- function(grid, dat, fn, ...) {
-  grid_types <- c("RasterLayer", "SpatialPolygonsDataFrame")
-  match.arg(class(grid), grid_types)
-  
+nomove_agg <- function(g, dat, fun, ...) {
   # raster aggregation just acts as a wrapper for raster::aggregate (but it's less difficult to work out)
-  if(class(grid) == "RasterLayer") {
-    a <- res(grid) / res(dat)
+  if("RasterLayer" %in% class(g)) {
+    a <- res(g) / res(dat)
     fun_args <- list(...)
-    cell_values <- raster::aggregate(dat, fact = a, fun=function(x, ...){do.call(get(fn), c(list(x), fun_args))})
+    out <- raster::aggregate(dat, fact = a, fun=function(x, ...){do.call(get(fun), c(list(x), fun_args))})
   } 
   
   # aggregation to a polygon does some cropping and calculating
-  if(class(grid) == "SpatialPolygonsDataFrame") {
-    cell_values <- sapply(1:nrow(grid), function(cell) {
-      grid_cell <- grid[cell, ]
+  if("sf" %in% class(g)) {
+    out <- furrr::future_map_dbl(st_geometry(g), function(grid_cell, dat, fun, ...) {
+      grid_cell <- grid_cell %>% sf::st_geometry() %>% sf::st_sf()
       dat_cell <- raster::crop(dat, grid_cell)
-      out <- get(fn)(as.vector(dat_cell), ...)
-    })
+      value <- get(fun)(as.vector(dat_cell), ...)
+    }, dat, fun, ...)
   }
-  return(cell_values)
+  return(out)
 }
