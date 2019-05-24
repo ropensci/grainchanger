72e7dacc977a4d45a33e538f67d86b20a3a74f89
laurajanegraham <laurajaneegraham@gmail.com>
2019-05-24 14:38:34 +0100

    fix nomove_agg shei example

--- R/nomove_agg.R
+++ R/nomove_agg.R
@@ -40,7 +40,7 @@
 #' d <- nomove_agg(g_sf, cont_ls, mean)
 #'
 #' # aggregate using Shannon evenness
-#' d <- nomove_agg(g_sf, cont_ls, nm_shei, lc_class = 0:3)
+#' d <- nomove_agg(g_sf, cont_ls, shei, lc_class = 0:3)
 #' @export
 
 nomove_agg <- function(coarse_dat, 
